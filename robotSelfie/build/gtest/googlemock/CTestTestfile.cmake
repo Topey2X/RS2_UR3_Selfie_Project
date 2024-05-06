23565586bc9a7a4ca58ec662594ee41acccb16fe
Darren Kikyanto <Darren.Kikyanto-1@student.uts.edu.au>
2024-05-06 16:01:43 +1000

    Symbolic Link Catkin Package

---
+++ robotSelfie/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/darren2004/catkin_ws/src/robotSelfie/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
